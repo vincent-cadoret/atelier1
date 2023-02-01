4c30f4693b70f7ea6c9d5620cc6fefddc74bc719
Vincent CADORET <vincent.cadoret@cpe.fr>
2023-02-01 14:48:35 +0100

    change java version

---
+++ src/main/java/com/grp1/atelier1/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.grp1.atelier1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+    @RequestMapping(value = { "/", "/index", "search" }, method = RequestMethod.GET)
+    public String searchCard() {
+        return "searchCard";
+    }
+    @RequestMapping(value = { "/add" }, method = RequestMethod.GET)
+    public String addCard() {
+        return "form-sample";
+    }
+}
