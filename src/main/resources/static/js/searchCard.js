748ef741134d16c0d1141e500b87289afc8dc28b
Vincent CADORET <vincent.cadoret@cpe.fr>
2023-02-01 16:40:22 +0100

    add search form

--- src/main/resources/static/js/searchCard.js
+++ src/main/resources/static/js/searchCard.js
@@ -1,11 +1,34 @@
-$('#cardFamilyImgId')[0].src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/DC_Comics_logo.png/280px-DC_Comics_logo.png";
-$('#cardFamilyNameId')[0].innerText="DC comics";
-$('#cardImgId')[0].src="http://www.guinnessworldrecords.com/images/superlative/superheroes/GWR-Superheroes-SUPERMAN.svg";
-$('#cardNameId')[0].innerText="SUPERMAN";
-$('#cardDescriptionId')[0].innerText="The origin story of Superman relates that he was born Kal-El on the planet Krypton, before being rocketed to Earth as an infant by his scientist father Jor-El, moments before Krypton's destruction. Discovered and adopted by a farm couple from Kansas, the child is raised as Clark Kent and imbued with a strong moral compass. Early in his childhood, he displays various superhuman abilities, which, upon reaching maturity, he resolves to use for the benefit of humanity through a 'Superman' identity.";
-$('#cardHPId')[0].innerText="50 HP";
-$('#cardEnergyId')[0].innerText="100 Energy";
-$('#cardAttackId')[0].innerText="17 Attack";
-$('#cardDefenceId')[0].innerText="80 Defence";
-
-
+$.ajax({
+    url: "http://vps.cpe-sn.fr:8083/card/8",
+    type: "GET",
+    success: function(data){
+        console.log(data);
+        $('#cardFamilyImgId')[0].src = data.family.img;
+        $('#cardFamilyNameId')[0].innerText=data.family.name;
+        $('#cardImgId')[0].src=data.img;
+        $('#cardNameId')[0].innerText=data.name;
+        $('#cardDescriptionId')[0].innerText=data.description;
+        $('#cardHPId')[0].innerText=data.hp;
+        $('#cardEnergyId')[0].innerText=data.energy;
+        $('#cardAttackId')[0].innerText=data.attack;
+        $('#cardDefenceId')[0].innerText=data.defence;
+    }
+});
+function loadCard(id){
+    $.ajax({
+        url: "http://vps.cpe-sn.fr:8083/card/" + id,
+        type: "GET",
+        success: function(data){
+            console.log(data);
+            $('#cardFamilyImgId')[0].src = data.family.img;
+            $('#cardFamilyNameId')[0].innerText=data.family.name;
+            $('#cardImgId')[0].src=data.img;
+            $('#cardNameId')[0].innerText=data.name;
+            $('#cardDescriptionId')[0].innerText=data.description;
+            $('#cardHPId')[0].innerText=data.hp;
+            $('#cardEnergyId')[0].innerText=data.energy;
+            $('#cardAttackId')[0].innerText=data.attack;
+            $('#cardDefenceId')[0].innerText=data.defence;
+        }
+    });
+}
