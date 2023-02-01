e476558515c87a1697bbad319dd6535283e29206
Vincent CADORET <vincent.cadoret@cpe.fr>
2023-02-01 14:40:46 +0100

    first commit

---
+++ src/main/java/com/grp1/atelier1/Atelier1Application.java
@@ -0,0 +1,11 @@
+package com.grp1.atelier1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atelier1Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Atelier1Application.class, args);
+	}
+}
---
+++ src/main/resources/templates/js/searchCard.js
@@ -0,0 +1,11 @@
+$('#cardFamilyImgId')[0].src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/DC_Comics_logo.png/280px-DC_Comics_logo.png";
+$('#cardFamilyNameId')[0].innerText="DC comics";
+$('#cardImgId')[0].src="http://www.guinnessworldrecords.com/images/superlative/superheroes/GWR-Superheroes-SUPERMAN.svg";
+$('#cardNameId')[0].innerText="SUPERMAN";
+$('#cardDescriptionId')[0].innerText="The origin story of Superman relates that he was born Kal-El on the planet Krypton, before being rocketed to Earth as an infant by his scientist father Jor-El, moments before Krypton's destruction. Discovered and adopted by a farm couple from Kansas, the child is raised as Clark Kent and imbued with a strong moral compass. Early in his childhood, he displays various superhuman abilities, which, upon reaching maturity, he resolves to use for the benefit of humanity through a 'Superman' identity.";
+$('#cardHPId')[0].innerText="50 HP";
+$('#cardEnergyId')[0].innerText="100 Energy";
+$('#cardAttackId')[0].innerText="17 Attack";
+$('#cardDefenceId')[0].innerText="80 Defence";
+
+
