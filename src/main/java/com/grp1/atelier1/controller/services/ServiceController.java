3211b0ab1063053ef0b858a2c2a3d1d9c7c15784
Sahamander <sahan69100@gmail.com>
2023-02-02 19:56:22 +0100

    ServiceController

---
+++ src/main/java/com/grp1/atelier1/controller/services/ServiceController.java
@@ -0,0 +1,37 @@
+package com.grp1.atelier1.controller.services;
+
+import ch.qos.logback.core.model.Model;
+import com.grp1.atelier1.controller.CardDAO;
+import com.grp1.atelier1.model.Card;
+import com.grp1.atelier1.model.CardFormDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ServiceController {
+    CardDAO cardDao = new CardDAO();
+    @GetMapping("/card/{id}")
+    public Card getCardbyId(@PathVariable("id") int id) {
+        return cardDao.getCardById(id);
+    }
+
+    @GetMapping("/card/random")
+    public Card getRandomCard() {
+        return cardDao.getRandomCard();
+    }
+
+    @GetMapping("/card/list")
+    public List<Card> getCardList() {
+        return cardDao.getCardList();
+    }
+
+    @RequestMapping(value = { "/addCard"}, method = RequestMethod.POST)
+    public String addcard(Model model, @ModelAttribute("cardForm") CardFormDTO cardForm) {
+        Card p = cardDao.addCard(cardForm.getId(), cardForm.getName(), cardForm.getDescription(), cardForm.getFamily(), cardForm.getAffinity(), cardForm.getImgUrl(), cardForm.getSmallImageUrl(),
+                cardForm.getEnergy(), cardForm.getHp(), cardForm.getDefence(), cardForm.getAttack(),
+                cardForm.getPrice(), cardForm.getUserId());
+        //model.addAttribute("myCard", p);
+        return "/card/random";
+    }
+}
