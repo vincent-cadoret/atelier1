6459e57c1118911d1c4d7ad0a8dbffc0dfef1bc9
mathysfr <matprofrancfort@gmail.com>
2023-02-05 22:53:34 +0100

    affichage

--- src/main/java/com/grp1/atelier1/controller/services/ServiceController.java
+++ src/main/java/com/grp1/atelier1/controller/services/ServiceController.java
@@ -1,36 +1,45 @@
 package com.grp1.atelier1.controller.services;
 
-import ch.qos.logback.core.model.Model;
+import org.springframework.ui.Model;
 import com.grp1.atelier1.controller.CardDAO;
 import com.grp1.atelier1.model.Card;
 import com.grp1.atelier1.model.CardFormDTO;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class ServiceController {
     CardDAO cardDao = new CardDAO();
     @GetMapping("/search")
-    public Card getCardById(@RequestParam("id") int id) {
-        return cardDao.getCardById(id);
+    public String getCardById(@RequestParam("id") int id, Model model) {
+        Card card = cardDao.getCardById(id);
+        model.addAttribute("card", card);
+        return "card";
+
     }
 
     @GetMapping("/random")
-    public Card getRandomCard() {
-        return cardDao.getRandomCard();
+    public String getRandomCard(Model model) {
+        Card card = cardDao.getRandomCard();
+        model.addAttribute("card", card);
+        return "card";
     }
 
     @GetMapping("/list")
-    public List<Card> getCardList() {
-        return cardDao.getCardList();
+    public String getCardList(Model model) {
+        List<Card> list = cardDao.getCardList();
+        model.addAttribute("list", list);
+        return "listCard";
     }
+
 
     @RequestMapping(value = { "/addCard"}, method = RequestMethod.POST)
     public String addcard(Model model, @ModelAttribute("cardForm") CardFormDTO cardForm) {
         Card p = cardDao.addCard(cardForm.getId(), cardForm.getName(), cardForm.getDescription(), cardForm.getFamily(), cardForm.getAffinity(), cardForm.getImgUrl(), cardForm.getSmallImageUrl(),
                 cardForm.getEnergy(), cardForm.getHp(), cardForm.getDefence(), cardForm.getAttack(),
                 cardForm.getPrice(), cardForm.getUserId());
-        return "redirect:/index";
+        return "redirect:searchCard";
     }
 }
